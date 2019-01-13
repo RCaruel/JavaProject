af417f1b25ad99968985a0c5521a995eb618c885
BSofianeB <45490012+BSofianeB@users.noreply.github.com>
2019-01-13 21:55:25 +0100

    ijij

--- src/mainPackage/Controleur.java
+++ src/mainPackage/Controleur.java
@@ -3,12 +3,10 @@ package mainPackage;
 public class Controleur {
 	
 	public Controleur() {
+		
         Fenetre f = new Fenetre();
-        //new ControlMouse(p,f);
-        //new ControlMenu(f);
         new ControlButton(f);
-       // f.setContentPane(new MoveComponents());
-        f.setVisible(true);
+       
     }
 
 }
